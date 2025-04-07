d9dae5274e9c1923dd98e4e8b46abfaeaab47a59
Samuel Cardenas <samcard1999@gmail.com>
2025-04-07 04:39:34 -0500

    first commit

--- src/components/Projects/Carousel.jsx
+++ src/components/Projects/Carousel.jsx
@@ -59,132 +59,138 @@ const Carousel = ({
         speed={550}
         observeSlideChildren={true}
         onSlideChange={(swiper) => {
-          const index = swiper.activeIndex + 1;
-          const cord = coordenadas.find((coord) => coord.id === index);
-          dispatch(oneFloor());
-          if (cord && cord.floor) {
-            dispatch(twoFloor());
-            if (cord.floor === 1 && floorCounter === 2) {
-              dispatch(decrement());
-              dispatch(
-                updateDimensions({ width: mapaWidthPx, height: mapaHeightPx })
-              );
-            } else if (cord.floor === 2 && floorCounter === 1) {
-              dispatch(increment());
+          if (coordenadas) {
+            const index = swiper.activeIndex + 1;
+            const cord = coordenadas.find((coord) => coord.id === index);
+            dispatch(oneFloor());
+            if (cord && cord.floor) {
+              dispatch(twoFloor());
+              if (cord.floor === 1 && floorCounter === 2) {
+                dispatch(decrement());
+                dispatch(
+                  updateDimensions({ width: mapaWidthPx, height: mapaHeightPx })
+                );
+              } else if (cord.floor === 2 && floorCounter === 1) {
+                dispatch(increment());
 
-              dispatch(
-                updateDimensions({
-                  width: floor_2_mapaWidthPx,
-                  height: floor_2_mapaHeightPx
-                })
-              );
+                dispatch(
+                  updateDimensions({
+                    width: floor_2_mapaWidthPx,
+                    height: floor_2_mapaHeightPx
+                  })
+                );
+              }
             }
-          }
 
-          const viewportWidth = window.innerWidth;
-          const viewportHeight = window.innerHeight;
+            const viewportWidth = window.innerWidth;
+            const viewportHeight = window.innerHeight;
 
-          let anchoDeseado = mapaWidthPx;
-          let largoDeseado = viewportHeight * 0.4;
-          if (viewportWidth < 767 && viewportWidth > 479) {
-            largoDeseado = viewportHeight * 0.3;
-          }
-          let anchoDeseado_pc = viewportWidth * 0.3;
-          let largoDeseado_pc = viewportHeight * 0.4;
+            let anchoDeseado = mapaWidthPx;
+            let largoDeseado = viewportHeight * 0.4;
+            if (viewportWidth < 767 && viewportWidth > 479) {
+              largoDeseado = viewportHeight * 0.3;
+            }
+            let anchoDeseado_pc = viewportWidth * 0.3;
+            let largoDeseado_pc = viewportHeight * 0.4;
 
-          const cordExist = coordenadas.find((coord) => coord.id === index);
+            const cordExist = coordenadas.find((coord) => coord.id === index);
 
-          if (viewportWidth < 767) {
-            anchoDeseado = viewportWidth * 0.9;
-          }
-          const escalaDeseada_ancho = anchoDeseado / mapaWidthPx;
-          const escalaDeseada_largo = largoDeseado / mapaHeightPx;
-          let crecimiento_px =
-            Math.min(escalaDeseada_ancho, escalaDeseada_largo) * mapaHeightPx -
-            mapaHeightPx;
-          const desplazamiento_texto = crecimiento_px + mapaHeightPx;
-          const margen_desplazamiento_px = 37;
-
-          if (cordExist) {
             if (viewportWidth < 767) {
-              gsap.to(".mapa", {
-                y: `${crecimiento_px / 2}px`,
-                scale: Math.min(escalaDeseada_ancho, escalaDeseada_largo),
-                opacity: 1,
-                duration: 0.5
-              });
-              gsap.to(".flex-item", {
-                y: `${desplazamiento_texto + margen_desplazamiento_px}px`,
-                duration: 0.5
-              });
-              document.querySelector(".project-container").style.height = `${
-                viewportHeight +
-                desplazamiento_texto +
-                margen_desplazamiento_px +
-                viewportHeight * 0.08
-              }px`;
-            } else {
-              const escalaDeseada_ancho_pc = anchoDeseado_pc / mapaWidthPx;
-              const escalaDeseada_largo_pc = largoDeseado_pc / mapaHeightPx;
-              const crecimiento_px_largo =
-                Math.min(escalaDeseada_ancho_pc, escalaDeseada_largo_pc) *
-                  mapaHeightPx -
-                mapaHeightPx;
-              const crecimiento_px_ancho =
-                Math.min(escalaDeseada_ancho_pc, escalaDeseada_largo_pc) *
-                  mapaWidthPx -
-                mapaWidthPx;
-
-              gsap.to(".mapa", {
-                y: `${-crecimiento_px_largo / 2}px`,
-                x: `${-crecimiento_px_ancho / 2}px`,
-                scale: Math.min(escalaDeseada_ancho_pc, escalaDeseada_largo_pc),
-                opacity: 1,
-                duration: 0.5
-              });
+              anchoDeseado = viewportWidth * 0.9;
             }
+            const escalaDeseada_ancho = anchoDeseado / mapaWidthPx;
+            const escalaDeseada_largo = largoDeseado / mapaHeightPx;
+            let crecimiento_px =
+              Math.min(escalaDeseada_ancho, escalaDeseada_largo) *
+                mapaHeightPx -
+              mapaHeightPx;
+            const desplazamiento_texto = crecimiento_px + mapaHeightPx;
+            const margen_desplazamiento_px = 37;
 
-            const shadowImg = document.querySelector(".shadow");
+            if (cordExist) {
+              if (viewportWidth < 767) {
+                gsap.to(".mapa", {
+                  y: `${crecimiento_px / 2}px`,
+                  scale: Math.min(escalaDeseada_ancho, escalaDeseada_largo),
+                  opacity: 1,
+                  duration: 0.5
+                });
+                gsap.to(".flex-item", {
+                  y: `${desplazamiento_texto + margen_desplazamiento_px}px`,
+                  duration: 0.5
+                });
+                document.querySelector(".project-container").style.height = `${
+                  viewportHeight +
+                  desplazamiento_texto +
+                  margen_desplazamiento_px +
+                  viewportHeight * 0.08
+                }px`;
+              } else {
+                const escalaDeseada_ancho_pc = anchoDeseado_pc / mapaWidthPx;
+                const escalaDeseada_largo_pc = largoDeseado_pc / mapaHeightPx;
+                const crecimiento_px_largo =
+                  Math.min(escalaDeseada_ancho_pc, escalaDeseada_largo_pc) *
+                    mapaHeightPx -
+                  mapaHeightPx;
+                const crecimiento_px_ancho =
+                  Math.min(escalaDeseada_ancho_pc, escalaDeseada_largo_pc) *
+                    mapaWidthPx -
+                  mapaWidthPx;
 
-            if (shadowImg) {
-              shadowImg.src = `/assets/images/mapas/${villa}/${index}.png`;
-            }
+                gsap.to(".mapa", {
+                  y: `${-crecimiento_px_largo / 2}px`,
+                  x: `${-crecimiento_px_ancho / 2}px`,
+                  scale: Math.min(
+                    escalaDeseada_ancho_pc,
+                    escalaDeseada_largo_pc
+                  ),
+                  opacity: 1,
+                  duration: 0.5
+                });
+              }
 
-            // Agregar animaciones a la línea de tiempo usando unidades relativas
+              const shadowImg = document.querySelector(".shadow");
 
-            gsap.to(".camera", {
-              opacity: 1,
-              duration: 0
-            });
-            tl.to(".shadow", {
-              opacity: 0,
-              duration: 0,
-              ease: "power2.out"
-            })
-              .to(".shadow", {
-                x: cord.x_shadow,
-                y: cord.y_shadow,
+              if (shadowImg) {
+                shadowImg.src = `/assets/images/mapas/${villa}/${index}.png`;
+              }
+
+              // Agregar animaciones a la línea de tiempo usando unidades relativas
+
+              gsap.to(".camera", {
+                opacity: 1,
+                duration: 0
+              });
+              tl.to(".shadow", {
+                opacity: 0,
                 duration: 0,
                 ease: "power2.out"
               })
-              .to(".camera", {
-                x: cord.x,
-                y: cord.y,
-                rotate: cord.a,
-                duration: 0.35,
-                ease: "power2.out"
-              })
-              .to(".shadow", {
-                opacity: 1,
-                duration: 0.25,
-                ease: "power2.out"
-              });
-          } else {
-            gsap.to(".mapa", { scale: 0, opacity: 0, duration: 0.5 });
-            if (window.innerWidth < 767) {
-              document.querySelector(".project-container").style.height =
-                "100vh";
-              gsap.to(".flex-item", { y: `0px`, duration: 0.5 });
+                .to(".shadow", {
+                  x: cord.x_shadow,
+                  y: cord.y_shadow,
+                  duration: 0,
+                  ease: "power2.out"
+                })
+                .to(".camera", {
+                  x: cord.x,
+                  y: cord.y,
+                  rotate: cord.a,
+                  duration: 0.35,
+                  ease: "power2.out"
+                })
+                .to(".shadow", {
+                  opacity: 1,
+                  duration: 0.25,
+                  ease: "power2.out"
+                });
+            } else {
+              gsap.to(".mapa", { scale: 0, opacity: 0, duration: 0.5 });
+              if (window.innerWidth < 767) {
+                document.querySelector(".project-container").style.height =
+                  "100vh";
+                gsap.to(".flex-item", { y: `0px`, duration: 0.5 });
+              }
             }
           }
         }}
